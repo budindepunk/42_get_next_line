56d212f75f67a6cb94e7e215fb4c3d11d967dbd2
budindepunk <carolinasilvarode@gmail.com>
2024-05-20 12:56:25 +0200

    2nd

--- get_next_line.c
+++ get_next_line.c
@@ -20,19 +20,23 @@ char	*get_next_line(int fd)
 {
 	static char	*remembrance;
 	char	*buff;
+	char	*line_raw;
 	char	*line;
 
-	remembrance = NULL;
 	buff = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buff)
 		return (NULL);
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
 		return (NULL);
-	line = read_to_line(fd, buff, remembrance); // stuff happens here
+	if (ft_strchr(remembrance, '\n') == NULL)
+		line_raw = read_to_line(fd, buff, remembrance);
+	else	
+		ft_strlcpy(line_raw, remembrance, ft_strlen(remembrance));
 	free(buff);
 	buff = NULL;
-	if (!line)
+	if (!line_raw)
 		return (NULL);
+	line = extract_line(line_raw, remembrance); /////
 	return (line);
 }
 
@@ -40,22 +44,36 @@ char	*get_next_line(int fd)
 // stop when the read segment contains \n or \0
 char	*read_to_line(int fd, char *buff, const char *remembrance)
 {
-	char	*line;
+	char	*line_raw;
+	size_t	bytes_read;
 
-	ft_strlcpy(line, "", 1);
-	if (remembrance != NULL)
-		line = ft_strjoin(line, remembrance);
+	bytes_read = 0;
+	line_raw = "";
+	if (ft_strlen(remembrance) > 0)
+		line_raw = ft_strjoin(line_raw, remembrance);
 	// and now i append to line one bufferito at a time
-	while (read(fd, buff, BUFFER_SIZE) > 0)
+	bytes_read = read(fd, buff, BUFFER_SIZE);
+	while (bytes_read > 0)
 	{
-		line = ft_strjoin(line, buff);
+		line_raw = ft_strjoin(line_raw, buff);
 		buff[BUFFER_SIZE] = '\0';
 		if (ft_strchr(buff, '\n'))
 			break ;
+		bytes_read = read(fd, buff, BUFFER_SIZE);
 	}
-	return (line);
+	line_raw[]
+	return (line_raw);
 }
 
-char	*set_remembrance();
+char	*extract_line(char *line_raw, char *remembrance)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	while (line_raw[i] != '\n' || line_raw[i] != '\0')
+		i++;
+	
+}
 // scan the line and take the remainder 
 // return (remembrance)

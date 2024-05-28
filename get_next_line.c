74d0ba10413f2816ac4db8be379c1b76dfc7a1b5
budindepunk <carolinasilvarode@gmail.com>
2024-05-28 10:15:08 +0200

    yeahhh

--- get_next_line.c
+++ get_next_line.c
@@ -1,51 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: csilva-r <csilva-r@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 10:13:56 by csilva-r          #+#    #+#             */
+/*   Updated: 2024/05/28 10:13:58 by csilva-r         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+static char	*read_to_line(int fd, char *line)
 {
-	static char 	*remembrance;
-	char		*buff;
-	char 		*line;
+	char	*buff;
 	int		bytes_read;
-	size_t 		i;
+	char	*temp;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
-	{
-		if (remembrance)
-		{
-			free(remembrance);
-			remembrance = NULL;
-		}
-		return (NULL);
-	}
 	buff = malloc((sizeof(char) * BUFFER_SIZE + 1));
 	if (!buff)
 		return (NULL);
-	if (!remembrance)
-		remembrance = ft_strdup("");
 	bytes_read = 1;
-	while (!ft_strchr(remembrance, '\n') && bytes_read != 0)
+	while (!ft_strchr(line, '\n') && bytes_read != 0)
 	{
 		bytes_read = read(fd, buff, BUFFER_SIZE);
 		if (bytes_read == -1)
 		{
 			free(buff);
-			free(remembrance);
+			free(line);
 			return (NULL);
 		}
 		buff[bytes_read] = '\0';
-		line = remembrance;
-		remembrance = ft_strjoin(line, buff);
-		free(line);
+		temp = line;
+		line = ft_strjoin(temp, buff);
+		free(temp);
 	}
-	free(buff);
-	line = ft_strdup(remembrance); 	// extract from remembrance
-	free(remembrance);
-	remembrance = NULL;
-	if (!line || ft_strlen(line) < 1)
-	{
-		free(line);
-		return (NULL);
-	}
+	free (buff);
+	return (line);
+}
+
+static char	*set_line_and_remember(char *line)
+{
+	size_t	i;
+	char	*remembrance;
+
 	i = 0;
 	while (line[i] != '\n' && line[i] != '\0')
 		i++;
@@ -57,5 +56,32 @@ char	*get_next_line(int fd)
 	}
 	if (line[i] != '\0')
 		line[i + 1] = '\0';
+	return (remembrance);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*remembrance;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	{
+		if (remembrance)
+		{
+			free(remembrance);
+			remembrance = NULL;
+		}
+		return (NULL);
+	}
+	if (!remembrance)
+		remembrance = ft_strdup("");
+	line = read_to_line(fd, remembrance);
+	remembrance = NULL;
+	if (!line || ft_strlen(line) < 1)
+	{
+		free(line);
+		return (NULL);
+	}
+	remembrance = set_line_and_remember(line);
 	return (line);
 }
--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -5,77 +5,83 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: csilva-r <csilva-r@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/05/23 16:40:12 by csilva-r          #+#    #+#             */
-/*   Updated: 2024/05/23 16:40:14 by csilva-r         ###   ########.fr       */
+/*   Created: 2024/05/28 10:14:12 by csilva-r          #+#    #+#             */
+/*   Updated: 2024/05/28 10:14:14 by csilva-r         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line_bonus.h"
 
-static char	*read_to_line(int fd, char *buff, char *remembrance)
+static char	*read_to_line(int fd, char *line)
 {
+	char	*buff;
 	int		bytes_read;
 	char	*temp;
 
+	buff = malloc((sizeof(char) * BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
 	bytes_read = 1;
-	while (bytes_read != 0)
+	while (!ft_strchr(line, '\n') && bytes_read != 0)
 	{
 		bytes_read = read(fd, buff, BUFFER_SIZE);
 		if (bytes_read == -1)
-			return (0);
-		else if (bytes_read == 0)
-			break ;
+		{
+			free(buff);
+			free(line);
+			return (NULL);
+		}
 		buff[bytes_read] = '\0';
-		if (!remembrance)
-			remembrance = ft_strdup("");
-		temp = remembrance;
-		remembrance = ft_strjoin(temp, buff);
+		temp = line;
+		line = ft_strjoin(temp, buff);
 		free(temp);
-		temp = NULL;
-		if (ft_strchr(buff, '\n'))
-			break ;
 	}
-	return (remembrance);
+	free (buff);
+	return (line);
 }
 
-static char	*set_remembrance(char *line_raw)
+static char	*set_line_and_remember(char *line)
 {
-	char	*remembrance;
 	size_t	i;
+	char	*remembrance;
 
 	i = 0;
-	while (line_raw[i] != '\n' && line_raw[i] != '\0')
+	while (line[i] != '\n' && line[i] != '\0')
 		i++;
-	remembrance = ft_substr(line_raw, i + 1, (ft_strlen(line_raw) - i));
-	if (i == ft_strlen(line_raw))
+	remembrance = ft_substr(line, i + 1, ft_strlen(line) - i);
+	if (i == ft_strlen(line))
 	{
 		free(remembrance);
 		remembrance = NULL;
 	}
-	if (line_raw[i] != '\0')
-		line_raw[i + 1] = '\0';
+	if (line[i] != '\0')
+		line[i + 1] = '\0';
 	return (remembrance);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*remembrance[MAX_ITEMS];
-	char		*buff;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	{
+		if (remembrance[fd])
+		{
+			free(remembrance[fd]);
+			remembrance[fd] = NULL;
+		}
 		return (NULL);
-	buff = (char *)malloc((sizeof(char) * BUFFER_SIZE + 1));
-	if (!buff)
-		return (NULL);
-	line = read_to_line(fd, buff, remembrance[fd]);
-	free(buff);
-	buff = NULL;
+	}
+	if (!remembrance[fd])
+		remembrance[fd] = ft_strdup("");
+	line = read_to_line(fd, remembrance[fd]);
+	remembrance[fd] = NULL;
 	if (!line || ft_strlen(line) < 1)
 	{
 		free(line);
 		return (NULL);
 	}
-	remembrance[fd] = set_remembrance(line);
+	remembrance[fd] = set_line_and_remember(line);
 	return (line);
 }
